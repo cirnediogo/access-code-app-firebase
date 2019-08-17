60b97cc5dbd94d6c43065bf5c9e581ca59c667e4
Diogo Cirne <cirnediogo@gmail.com>
2019-08-16 21:45:08 -0300

    Added email sending functionality.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
-const functions = require('firebase-functions');
+'use strict';
+const functions  = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({origin: true});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+let url = "smtps://<EMAIL>%40gmail.com:"+encodeURIComponent('<SENHA>') + "@smtp.gmail.com:465";
+let transporter = nodemailer.createTransport(url);
+
+exports.enviarEmail = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    let remetente = '"REMETENTE" <EMAIL@gmail.com>';
+
+    let assunto = req.body['assunto'];
+    let destinatarios = req.body['destinatarios']; // lista de e-mails destinatarios separados por ,
+    let corpo = req.body['corpo'];
+    let corpoHtml = req.body['corpoHtml'];
+
+    let email = {
+        from: remetente,
+        to: destinatarios,
+        subject: assunto,
+        text: corpo,
+        html: corpoHtml
+    };
+
+    transporter.sendMail(email, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('Mensagem %s enviada: %s', info.messageId, info.response);
+    });
+  });
+});
